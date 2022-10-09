b3f2ea855ae70c321965082350207e72f03b915e
SiKion <muravy40020182@mail.ru>
2022-10-09 16:06:05 +0300

    test pokemon

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+
+import ru.ifmo.se.pokemon.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Battle b = new Battle();
+        Pokemon p1 = new Pokemon("Чужой", 1);
+        Pokemon p2 = new Pokemon("Хищник", 1);
+        b.addAlly(p1);
+        b.addFoe(p2);
+        b.go();
     }
 }
