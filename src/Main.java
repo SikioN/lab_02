d2a34b3a5e35021027f28a957945737b5ec74cb3
SiKion <muravy40020182@mail.ru>
2022-10-11 18:54:16 +0300

    add random_level

--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,14 @@ import static java.lang.Math.random;
 
 public class Main {
     public static void main(String[] args) {
-        final int MIN = 1, MAX = 100;
-
-
         Battle battle = new Battle();
-        Pokemon dedenne = new Dedenne("Dedenne", MIN + (int) (random() * (MAX - MIN) + 1));
-        Pokemon onix = new Onix("Onix", MIN + (int) (random() * (MAX - MIN) + 1));
-        Pokemon steelix = new Steelix("Steelix", MIN + (int) (random() * (MAX - MIN) + 1));
+        Pokemon dedenne = new Dedenne("Деденне", random_level());
+        Pokemon onix = new Onix("Оникс", random_level());
+        Pokemon steelix = new Steelix("Стиликс", random_level());
 
-        Pokemon tynamo = new Tynamo("Tynamo", MIN + (int) (random() * (MAX - MIN) + 1));
-        Pokemon eelektrik = new Eelektrik("Eelektrik", MIN + (int) (random() * (MAX - MIN) + 1));
-        Pokemon eelektross = new Eelektross("Eelektross", MIN + (int) (random() * (MAX - MIN) + 1));
+        Pokemon tynamo = new Tynamo("Тинамо", random_level());
+        Pokemon eelektrik = new Eelektrik("Илектрик", random_level());
+        Pokemon eelektross = new Eelektross("Илектросс", random_level());
 
         battle.addAlly(dedenne);
         battle.addAlly(onix);
@@ -25,7 +22,12 @@ public class Main {
         battle.addFoe(eelektrik);
         battle.addFoe(eelektross);
 
-        battle.go();
 
     }
+
+    static int random_level() {
+        final int MIN = 1, MAX = 100;
+        return MIN + (int) (random() * (MAX - MIN) + 1);
+    }
+
 }
